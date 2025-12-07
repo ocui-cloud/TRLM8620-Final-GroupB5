2799cc56fc65bf67aa2b647d6378d41a76287e79
ocui-cloud <ocui@middlebury.edu>
2025-12-06 20:08:46 -0800

    Use i18n.formatCurrency in order history

--- src/views/pages/OrderHistory.js
+++ src/views/pages/OrderHistory.js
@@ -1,11 +1,7 @@
 import { orderHistory } from "../../app.js";
 import i18n from "../../services/i18n.js";
 
-//takes a number and adds commas to it every 3 digits
-//VERY BAD i18n
-let formatCurrencyWithCommas = (x) => {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+
 
 let OrderHistory = {
 
@@ -35,8 +31,7 @@ let OrderHistory = {
                     <h3>${order.getOrderDate()}</h3>
                     <h3>${order.orderNumber}</h3>
                     <div class="gridPrice">
-                        $
-                        ${formatCurrencyWithCommas(order.total)}
+                        ${i18n.formatCurrencyWithCommas(order.total)}
                     </div>
                     <h3>${order.getOrderStatus()}</h3>
                 </article>`
